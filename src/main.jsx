cb7c42046b5c67a528c8273dcefd541aef201f50
Jimmymun33 <imp.awareness@gmail.com>
2026-05-13 13:52:01 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WokThinking from './WokThinking.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WokThinking />
+  </React.StrictMode>,
+)
+
